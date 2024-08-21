77484a60f977746b8a31137d92df6ab44316f202
Priya Bhoomireddi <priyagetlucky1011@gmail.com>
2024-08-21 22:25:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    }
 
-class Square extends Animal {}
+    // Getter for width
+    get width() {
+        return this._width;
+    }
+
+    // Getter for height
+    get height() {
+        return this._height;
+    }
+
+    // Method to calculate and return the area of the rectangle
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+class Square extends Rectangle {
+    constructor(side) {
+        // Call the Rectangle constructor with side as both width and height
+        super(side, side);
+    }
+
+    // Method to calculate and return the perimeter of the square
+    getPerimeter() {
+        return 4 * this.width;
+    }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
